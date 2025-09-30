5202d8d86bb71223bae1485f8096c2a7686bef9b
Iván Gómez <xigomjim596@ieshnosmachado.org>
2025-09-30 20:16:32 +0200

    trabajo hecho

--- diccionariosPractica.py
+++ diccionariosPractica.py
@@ -1,4 +1,3 @@
-
 # nombre = input("Introduce el nombre del videojuego: ")
 # valoracion = input("Introduce la valoracion del videojuego: ")
 # categoria = input("Introduce la categoria del videojuego: ")
@@ -13,7 +12,7 @@ nVideojuegos = int(input("Introduce el numero de videojuegos: "))
 
 for i in range(nVideojuegos):
     nombre = input("Introduce el nombre del videojuego: ")
-    valoracion = input("Introduce la valoracion del videojuego: ")
+    valoracion = int(input("Introduce la valoracion del videojuego: "))
     categoria = input("Introduce la categoria del videojuego: ")
     videojuego = {'nombre': nombre, 'valoracion': valoracion, 'categoria': categoria}
     videojuegos.append(videojuego)
@@ -23,9 +22,19 @@ print(videojuegos)
 
 for juego in videojuegos:
     print(juego["nombre"])
-    print("Media de todas las valoraciones: ", sum(juego["valoracion"] / len(videojuegos)))
-
-    
 
 
+#Tarea:
+
+print("Media de todas las valoraciones: ", sum([juego["valoracion"] for juego in videojuegos])/len(videojuegos))
+
+#Crear lista en una sola linea con todos los nombres de los videojuegos
+
+nombreJuegos = [juego.get("nombre") for juego in videojuegos]
+print(nombreJuegos)
+
+#Crear lista en una sola linea con todos las valoraciones de los videojuegos
+
+valoracionesJuegos = [juego.get("valoracion") for juego in videojuegos]
+print(valoracionesJuegos)
 
